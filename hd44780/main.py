5657cf9fdca9426ce7096587de5b4f648829e85e
Andreas Böttger <andy.boettger@gmail.com>
2016-04-30 23:06:24 +0200

    Initial commit

---
+++ hd44780/main.py
@@ -0,0 +1,13 @@
+import lcddriver
+import time , commands, re
+
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
+while True:
+  zeit = time.strftime("%y. %b    %H:%M")
+  out = commands.getoutput("su -c 'doveadm mailbox status all INBOX' aboettger")
+  m = re.search('.*unseen=(.+?) .*', out)
+  inbox = "{:<15}".format(m.group(1))
+  lcd.lcd_display_string ("%s" %(zeit), 1)
+  lcd.lcd_display_string ("%s %s" %("INBOX:", inbox), 2)
+  time.sleep (10)
